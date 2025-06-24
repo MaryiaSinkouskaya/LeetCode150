3ecf0dcea1313df871d3544b2f77b7836ba00106
Maryia Sinkouskaya <masha25112000@gmail.com>
2025-06-24 16:36:48 +0300

    task2

--- src/section1_arrays_strings/RemoveElement.java
+++ src/section1_arrays_strings/RemoveElement.java
@@ -2,7 +2,25 @@ package section1_arrays_strings;
 
 class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        // Your solution here
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                k++;
+                moveElements(nums, i);
+                i--;
+            }
+        }
+        return nums.length - k;
+    }
+
+    private void moveElements(int[] nums, int pos){
+        for (int i = pos; i < nums.length - 1; i++) {
+            nums[i] = nums[i+1];
+        }
+        nums[nums.length-1] = Integer.MAX_VALUE;
     }
 } 
