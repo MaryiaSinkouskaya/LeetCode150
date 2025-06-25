712296e2d95c1c395f42229358cd8aa22b175803
Maryia Sinkouskaya <masha25112000@gmail.com>
2025-06-25 11:54:46 +0300

    task4

--- src/section1_arrays_strings/RemoveDuplicatesFromSortedArrayII.java
+++ src/section1_arrays_strings/RemoveDuplicatesFromSortedArrayII.java
@@ -1,8 +1,21 @@
 package section1_arrays_strings;
 
 class RemoveDuplicatesFromSortedArrayII {
+
+
+    // Example 2: nums = [0, 0,1, 0, 0,1,1,1,1,2,3,3]
     public int removeDuplicates(int[] nums) {
-        // Your solution here
-        return 0;
+        if (nums.length <= 2){
+            return nums.length;
+        }
+
+        int pos = 2;
+        for(int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[pos - 2]) {
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        return pos;
     }
 } 
